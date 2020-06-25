1414c4959a706586069131ebaad805349955a6b3
MikeFranco <mfranco_98@yahoo.com>
2020-06-25 15:14:14 -0500

    added my home dropdown in av bar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,11 +1,41 @@
 <template>
   <v-toolbar color="#555582" class="toolbar">
-    
+    <v-menu :nudge-width="100">
+      <template v-slot:activator="{ on }">
+        <v-toolbar-title v-on="on">
+          <span>Mi hogar</span>
+          <v-icon dark>mdi-menu-down</v-icon>
+        </v-toolbar-title>
+      </template>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item v-for="item in items" :key="item">
+            <v-list-item-content>
+              <v-list-item-title v-text="item"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-menu>
   </v-toolbar>
 </template>
 
 <script>
-export default {};
+import BtnIcon from './BtnIcon.vue';
+export default {
+  components: {
+    BtnIcon
+  },
+  data() {
+    return {
+      items: [
+        'Salas',
+        'Dispositivos',
+        'Historial'
+      ]
+    };
+  }
+};
 </script>
 
 <style>
